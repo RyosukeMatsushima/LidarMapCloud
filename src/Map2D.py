28461f80917c85e85bebc732469193ca7d0c45e7
RyosukeMatsushima <m.ryosuke@me.com>
2021-06-05 01:52:58 +0900

    fix - angle_to_pix logic

--- src/Map2D.py
+++ src/Map2D.py
@@ -7,7 +7,8 @@ from .Axis import Axis
 from . import LidarSpec
 
 class Map2D:
-    # map resolution is number of pixels in 1[m] or 2pi[rad]
+    # map resolution means of pixels in 1[m] or 2pi[rad]
+    # north angle is 0[rad] and 0 <= angle < 2pi
     # size[m]
     def __init__(self, map_XY_resolution: int, map_angle_resolution: int, size: int):
 
@@ -74,40 +75,4 @@ class Map2D:
         return pix
 
     def angle_to_pix(self, angle):
-        return int(angle / self.angle_resolution)
-
-    # def __init__(self, axisX: Axis, axisY: Axis, axisTheta: Axis, axisDistance: Axis):
-    #     self.axisX = axisX
-    #     self.axisY = axisY
-    #     self.axisTheta = axisTheta
-    #     self.axisDistance = axisDistance
-
-    #     filename = path.join(mkdtemp(), 'likelihood_data.dat')
-    #     self.likelihood_data = np.memmap(filename, dtype='float32', mode='w+', shape=(len(axisX.elements), len(axisY.elements), len(axisTheta.elements), len(axisDistance.elements)))
-
-    #     filename = path.join(mkdtemp(), 'data_count.dat')
-    #     self.data_count = np.memmap(filename, dtype='int64', mode='w+', shape=(len(axisX.elements), len(axisY.elements)))
-
-    #     self._scan_data_set = [] # [[X, Y, angle, distance, weight]] of robot
-
-    #     self.normal_diffusion = self.get_normal_diffusion()
-
-    # def get_normal_diffusion(self):
-    #     return np.random.normal(0, 1, (400, 2)) * np.array([LidarSpec.ANGLE_RESOLUTION, LidarSpec.DISTANCE_RESOLUTION(1.)])
-
-    # def add_data(self, X, Y, angle, distance, weight):
-    #     self._scan_data_set.append(X, Y, angle, distance, weight])
-
-    # def calculate_likelihood_around(self, X, Y):
-
-    # def calculate_likelihood(self, X, Y):
-    #     pos = np.array([X, Y])
-    #     num_X = self.axisX.val2num(X)
-    #     num_Y = self.axisY.val2num(Y)
-    #     for scan_data in self._scan_data_set[self.data_count[num_X][num_Y]:]:
-    #         scan_pos = np.array([scan_data[0], scan_data[1]])
-    #         angle = scan_data[2]
-    #         distance = scan_data[3]
-    #         weight = scan_data[4]
-
-    #         angle2scanner = np.acos()
+        return int(angle / np.pi * self.angle_resolution)
